8abde79c667be0abd5c456967c723ea87a25ea52
liujiaan <liujiaan@jd.com>
2023-01-15 10:39:30 +0800

    blog update

---
+++ statics/marry/js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $(function() {
+
+        $('#sb-slider').slicebox({
+            slicesCount : 15,
+            sequentialRotation : true,
+            sequentialFactor : 50,
+            speed3d : 250,
+            cuboidsRandom : true
+        });
+        if( !Modernizr.csstransforms3d ) {
+            $('#sb-note').show();
+            $('body').append(
+                $('script').attr( 'type', 'text/javascript' ).attr( 'src', 'lib/jquery.easing.1.3.js' ));
+        }
+    });
+    //音乐播放器
+    var ap = new APlayer({
+        element: document.getElementById('bg-player'),
+        narrow: true,
+        autoplay: true,
+        showlrc: false,
+        mutex: true,
+        playlist: false,
+        theme: '#e6d0b2',
+        music: [{
+            title: '',
+            author: '',
+            url: 'http://oss.liujiaan.top/audio/hexo/2016/Bruno%20Mars%20-%20Marry%20You.mp3',
+            pic: 'http://oss.liujiaan.top/image/hexo/Preparation.jpg'
+        }, {
+            title: '',
+            author: '',
+            url: 'http://oss.liujiaan.top/audio/hexo/2016/Stewart%20Mac%20-%20I%20Love%20You.mp3',
+            pic: 'http://oss.liujiaan.top/image/hexo/Preparation.jpg'
+        }]
+    });
+    ap.volume(0.2);//音量
+});
